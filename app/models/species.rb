f65fe7bc280b36923ba7bf8c7143406a78bc9c30
Tecksian <Collins.Austin@gmail.com>
2016-02-16 15:11:21 -0500

    Added validation on name (see comments), removed controller and spec files.

--- app/models/species.rb
+++ app/models/species.rb
@@ -1,2 +1,8 @@
+# A model for species -- however, we are treating this as reference data, like a dictionary.
+# Species data will not be (hopefully!) changeable via the API, and populated merely as seed data.
 class Species < ActiveRecord::Base
+  # Though we don't formally need to validate this data as no changes
+  # are *intended* to be made to it, it's useful for helping us find our own
+  # errors.
+  validates :name, presence: :true, uniqueness: :true
 end
