6d4994e5950c7a5b5696763dcc659ea1940d79c9
Tecksian <Collins.Austin@gmail.com>
2016-02-16 18:15:05 -0500

    Serialized the dinosaur model, modified the controller to pass tests. Tests. pass.

--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -1,3 +1,5 @@
+include ActiveModel::Serializers::JSON
+
 # Dinosaur class. Only fundamental attributes are name, cage_id, and species_id. Created method-based
 # attributes for species_name, is_carnivore, and allowed viewing of cage_id which is functionally similar
 # to a cage number.
@@ -9,18 +11,25 @@ class Dinosaur < ActiveRecord::Base
             # We choose the easier of the two implied options, if only to avoid sitcom-like confusion!
             uniqueness: true
   # dinosaurs must have a species....
-  belongs_to :species, inverse_of: :dinosaurs, validate: true
+  validates :species, presence: :true
   # ... and moreover they must have a VALID species. Despite my 'Testasaurus' spec names.
+  belongs_to :species, inverse_of: :dinosaurs, validate: true
+
 
   # dinosaurs needn't have a cage, but if they do, it needs to be a real cage.
   belongs_to :cage, inverse_of: :dinosaurs, validate: true
+  attr_accessor :species_name, :is_carnivore
+  # overloading attributes is the crux of ActiveModel::Serializers
+  def attributes
+    {name: self.name, species_name: species_name, is_carnivore: is_carnivore, cage_id: self.cage_id}
+  end
 
   def species_name
-    @species_name = self.species.name
+    self.species.name unless species_id.nil?
   end
 
   def is_carnivore
-    @is_carnivore = self.species.is_carnivore
+    self.species.is_carnivore unless species_id.nil?
   end
 
 end
