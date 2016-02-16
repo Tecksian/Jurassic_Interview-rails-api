5cd7daf71a4d729b000406ec94b475ddb9cf055e
Tecksian <Collins.Austin@gmail.com>
2016-02-16 13:35:19 -0500

    Configured to use RocketPants. Let's see what it's got....

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
-class ApplicationController < ActionController::API
+# Configured to use RocketPants. Why use RocketPants? Mostly because
+# I've always wanted to try it out. Plus, some of the JSON and error
+# handling / testing functionality sounded useful.
+class ApplicationController < RocketPants::Base
 end
