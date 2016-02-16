9940f52dd845c030cacde736316da43eeb25e6a4
Tecksian <Collins.Austin@gmail.com>
2016-02-16 16:59:50 -0500

    Created the dinosaurs/cage route to the dinosaurs#cage method. You can now assign dinosaurs to cages, essentially by using a PUT request to modify their cage_id. More to come.

--- app/controllers/dinosaurs_controller.rb
+++ app/controllers/dinosaurs_controller.rb
@@ -1,5 +1,5 @@
 class DinosaursController < ApplicationController
-  before_action :set_dinosaur, only: [:show, :update, :destroy]
+  before_action :set_dinosaur, only: [:show, :update, :destroy, :to_cage]
 
   # GET /dinosaurs
   # GET /dinosaurs.json
@@ -14,6 +14,27 @@ class DinosaursController < ApplicationController
   def show
     expose @dinosaur
   end
+
+  def to_cage
+    @cage = Cage.find(cage_params[:id])
+    # As we're using validation on the Cage model to check for error
+    # conditions, we cannot simply do either:
+    #   @dinosaur.cage = @cage as this would not update the cage's number of dinosaurs until after @dinosaur is saved,
+    #   @cage.dinosaurs << @dinosaur as this would autosave the dinosaur before running Cage validations.
+    # Instead, we need to add @dinosaur to the Cage's associaton proxy
+    @cage.dinosaurs(true).target.append(@dinosaur)
+
+    @dinosaur.cage_id=@cage.id
+    #run the cage validations to make sure the dinosaur can go into the cage
+    if @cage.invalid?
+      error!(:invalid_resource, @cage.errors)
+    elsif @dinosaur.save
+      expose @dinosaur, {root: :true, except: [:created_at, :updated_at]}
+    else
+      error!(:invalid_resource, @dinosaur.errors)
+    end
+  end
+
 
   # POST /dinosaurs
   # POST /dinosaurs.json
@@ -53,14 +74,18 @@ class DinosaursController < ApplicationController
   private
 
   def set_dinosaur
-    @dinosaur = Dinosaur.find(params[:id])
+    @dinosaur = Dinosaur.find(params[:dinosaur][:id])
   end
 
   def dinosaur_params
-    params.require(:dinosaur).permit(:name, :species_id)
+    params.require(:dinosaur).permit(:name, :species_id, :cage_id)
   end
 
   def species_params
-    params[:dinosaur].require(:species).permit(:id, :name)
+    params[:dinosaur].require(:species).permit(:id)
+  end
+
+  def cage_params
+    params[:dinosaur].require(:cage).permit(:id)
   end
 end
