59bb8530086f5c6fe32b9e711d7aaecce86f3974
Tecksian <Collins.Austin@gmail.com>
2016-02-16 13:55:42 -0500

    basic factory for generating dinosaurs.

---
+++ spec/factories/dinosaurs.rb
@@ -0,0 +1,10 @@
+# For an application this size/complexity, probably faster not to use FactoryGirl.
+# However, I'd like to give the impression of what I'd do on a more complex project,
+# so I'll go ahead and crack this peanut with a sledgehammer. Plus, it DRYs up code nicely.
+FactoryGirl.define do
+  # create a dinosaur for our testing, with a sequential name.
+  factory :dinosaur do
+    # sequences are good for avoiding unique constraints!
+    sequence(:name){ |index| "Testasaurus-#{index}"}
+  end
+end
