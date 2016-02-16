376e959518834bbd94f7a5ffa6b9d2faa516d963
Tecksian <Collins.Austin@gmail.com>
2016-02-16 17:49:09 -0500

    Added some method-derived attributes for easier user interface. Nested JSON works, but users will make less mistakes the easier we make it. Will we make more mistakes because of it? Um....nah?

--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -1,4 +1,6 @@
-# Right now, Dinosaurs only have a name.
+# Dinosaur class. Only fundamental attributes are name, cage_id, and species_id. Created method-based
+# attributes for species_name, is_carnivore, and allowed viewing of cage_id which is functionally similar
+# to a cage number.
 class Dinosaur < ActiveRecord::Base
   validates :name,
             presence: true,
@@ -6,8 +8,19 @@ class Dinosaur < ActiveRecord::Base
             # is name+species being unique (otherwise nonsense ensues -- in business use, not only code).
             # We choose the easier of the two implied options, if only to avoid sitcom-like confusion!
             uniqueness: true
-
+  # dinosaurs must have a species....
   belongs_to :species, inverse_of: :dinosaurs, validate: true
-  belongs_to :cage, inverse_of: :dinosaurs, validate: true
-end
+  # ... and moreover they must have a VALID species. Despite my 'Testasaurus' spec names.
 
+  # dinosaurs needn't have a cage, but if they do, it needs to be a real cage.
+  belongs_to :cage, inverse_of: :dinosaurs, validate: true
+
+  def species_name
+    @species_name = self.species.name
+  end
+
+  def is_carnivore
+    @is_carnivore = self.species.is_carnivore
+  end
+
+end
